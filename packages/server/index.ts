ff967f76cb04267f5b85659229e62c31aea91432
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-08-21 23:01:45 +0300

    Extracting Conversation Repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
+import express from 'express';
 import OpenAI from 'openai';
 import z from 'zod';
+import { conversationRepository } from './repositories/conversation.repository';
 
 dotenv.config();
 
@@ -17,9 +18,6 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello World!');
 });
-
-// conversationId -> lastResponseId
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -48,9 +46,10 @@ app.post('/api/chat', async (req: Request, res: Response) => {
          input: prompt,
          temperature: 0.2,
          max_output_tokens: 100,
-         previous_response_id: conversations.get(conversationId),
+         previous_response_id:
+            conversationRepository.getLastResponseId(conversationId),
       });
-      conversations.set(conversationId, response.id);
+      conversationRepository.setLastResponseId(conversationId, response.id);
 
       res.json({ message: response.output_text });
    } catch (error) {
